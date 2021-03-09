492f1d3d5a2ed048ec867dbe560225f293cd2392
Toxumuharu <ring.o.awlright@gmail.com>
2021-03-09 22:56:43 +0900

    add example

--- CustomTextFieldList_SwiftUI/ContentView.swift
+++ CustomTextFieldList_SwiftUI/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var arr: [String] = [""]
+    @State var new = ""
+
+    @State private var numberOfCells = 5
+    @State private var currentlySelectedCell: Int = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            NavigationView{
+                List{
+                    ForEach(0 ..< self.numberOfCells) { index in
+                        CharacterInputCell(currentlySelectedCell: self.$currentlySelectedCell, index: index)
+                            .frame(maxHeight: UIScreen.screenHeight * 0.08)
+                    }
+                }.navigationTitle("TextFieldList")
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ README.md
@@ -0,0 +1,22 @@
+# CustomTextFieldList_SwiftUI
+> List with TextField.
+
+[![Generic badge](https://img.shields.io/badge/version-1.0.0-green.svg)](https://shields.io/)
+
+List with TextField. If you push Enter in TextField, the cursor will move to next TextField automatically.
+
+# Application Image
+
+[](![CustomTextFieldList_SwiftUI_v_1_0_0.gif](ReadmeContents/CustomTextFieldList_SwiftUI_v_1_0_0.gif))
+<p align="center"> <img src="ReadmeContents/CustomTextFieldList_SwiftUI_v_1_0_0.png" alt="CustomTextFieldList_SwiftUI_v_1_0_0" width="250" border="1"/> </p>
+
+
+## Release History
+
+* 1.0.0
+    * The first proper release
+    * Just for example
+
+## Meta
+
+Tomoharu - [https://github.com/Toxumuharu](https://github.com/Toxumuharu)

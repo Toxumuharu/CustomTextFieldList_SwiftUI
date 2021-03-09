f7c6512be183786bb921e3d2a1322b86312bc2c6
Toxumuharu <ring.o.awlright@gmail.com>
2021-03-09 21:07:58 +0900

    Initial Commit

---
+++ CustomTextFieldList_SwiftUI/CustomTextFieldList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldList_SwiftUIApp.swift
+//  CustomTextFieldList_SwiftUI
+//
+//  Created by Toxumuharu on 2021/03/09.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
